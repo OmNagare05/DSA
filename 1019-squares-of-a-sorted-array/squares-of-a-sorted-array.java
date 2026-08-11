4e5602404960550c0625f683f146c5b7cde80170
OmNagare05 <nagareom05@gmail.com>
2026-08-11 10:52:38 +0530

    Time: 166 ms (5.00%) | Memory: 48.4 MB (14.56%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,21 +1,101 @@
 class Solution {
-    public static int[] sortedSquares(int[] nums) {
+    public int[] sortedSquares(int[] nums) {
 
-
-        int newarr[] = new int[nums.length];
        
+        int negativelength =0;
+        int positivelength = 0; 
+        
+
         for(int i=0; i<nums.length; i++){
 
-            newarr[i] = nums[i] * nums[i];
-           
-
+            if(nums[i] < 0){
+               negativelength++;
+            }else{
+               positivelength++;
+            }
         }
-         Arrays.sort(newarr);
+        int a[] = new int[negativelength];  // negative no is st
+        int b[] = new int[positivelength];  // positive no is store
+        int indx1 =0;
+        int indx2 =0;
+
+       for(int i=0; i<nums.length; i++){
+        if(nums[i] >=0){
+            b[indx1] = nums[i];
+            indx1++;
+        }else{
+            a[indx2] = nums[i];
+            indx2++;
+        }
+
+       }
+       if(a.length == 0){   // no negative value in nums array
+           for(int i=0; i<nums.length; i++){
+             nums[i] = nums[i] * nums[i];
+           }
+           return nums;
+       }
+       if(b.length ==0){   // no postive value in nums array
+        for(int i=0; i<nums.length; i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        int left =0;
+        int right = nums.length-1;
+        while(left < right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+        return nums;
+
+       }
+       for(int i=0; i<a.length; i++){
+        a[i] = a[i] * a[i];
+       }
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<a.length-1-i; j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp; 
+                }
+            }
+        }
+       for(int i=0; i<b.length; i++){
+        b[i] = b[i] * b[i];
+       }
+
+
+        int i=0; 
+        int j=0; 
+        int newarr[] = new int[positivelength + negativelength];
+        int indx=0;
+        while(i< negativelength && j<positivelength){
+
+            if(a[i] < b[j]){
+                  newarr[indx] = a[i];
+                  i++;
+                  indx++;
+                
+            }else{
+                newarr[indx] = b[j];
+                indx++;
+                j++;
+            }
+        }
+        while(i<negativelength){
+            newarr[indx] = a[i];
+            indx++;
+            i++;
+        }
+        while(j<positivelength){
+            newarr[indx] = b[j];
+            indx++;
+            j++;
+        }
         return newarr;
         
-    }
-    public static void main(String args[]){
-        int nums[] = {-4,-1,0,3,10};
-        System.out.println(Arrays.toString(sortedSquares(nums)));
     }
 }
