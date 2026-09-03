d0f8f335af58f008418aa3d2eb5fd68e260da54f
OmNagare05 <nagareom05@gmail.com>
2026-09-03 21:47:16 +0530

    Time: 1 ms (97.29%) | Memory: 47.6 MB (22.66%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+        int left =0;
+        int sum  =0;
+        int right =0;
+        int n = nums.length;
+        for(int i=0; i<n ; i++){
+            sum = sum +nums[i];
+           
+        }
+
+        for(int i=0; i<n; i++){
+           
+             right = sum - left - nums[i];
+
+            if(left == right){
+                return i;
+            }
+             left = left + nums[i];
+                
+            
+        }
+        return -1;
+        
+    }
+}
